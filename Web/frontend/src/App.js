36862ce393c72f49c149e58e4eab4c346a170fc8
Guilherme Yabu <guilhermeyabu@MacBook-Pro-de-Guilherme.local>
2020-04-15 11:31:07 -0300

    Aula 03: Criação página de login

--- Web/frontend/src/App.js
+++ Web/frontend/src/App.js
@@ -1,19 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import Header from './Header';
+import Logon from './pages/Logon';
+
+import './global.css';
 
 function App() {
   // Array [valor, funcao de atualizacao]
-  const [counter, setCounter] = useState(0);
-
-  function increment() {
-    setCounter(counter + 1);
-  }
+  //const [counter, setCounter] = useState(0);
 
   return (
     <div>
-      <Header>Contador: {counter}</Header>
-      <button onClick={increment}>Incrementar</button>
+      <Logon />
     </div>
 
   );
