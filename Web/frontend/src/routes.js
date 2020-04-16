f7046f8b469c1d71b8f38245e7d54cb7cd580302
Guilherme Yabu <guilhermeyabu@MacBook-Pro-de-Guilherme.local>
2020-04-16 16:03:24 -0300

    Aula 03: Criação da página novos casos

--- Web/frontend/src/routes.js
+++ Web/frontend/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Logon from './pages/Logon';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
 
 /**
  * Switch garante apenas um rota sendo chamada.
@@ -15,6 +16,7 @@ export default function Routes() {
                 <Route path="/" exact component={Logon} />
                 <Route path="/register" component={Register} />
                 <Route path="/profile" component={Profile} />
+                <Route path="/incidents/new" component={NewIncident} />
             </Switch>
         </BrowserRouter>
     );
